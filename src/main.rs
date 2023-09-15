3db712753b14eab2eaac6aa0690be365d5720c33
Taha Ahmadi <taha.ahmadi.wo@gmail.com>
2023-09-15 15:40:38 +0330

    - Add Termion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+extern crate termion;
+
+use termion::color;
+
+
+fn main(){
+    println!("{red}more red than any comrade{reset} test and {yellow} yellow now",
+    red   = color::Fg(color::Red),
+    reset = color::Fg(color::Reset),
+    yellow = color::Fg(color::Yellow));
 }
