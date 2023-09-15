5772ab4578638a39ebcc4c3a7e2a948253c9b2ee
Taha Ahmadi <taha.ahmadi.wo@gmail.com>
2023-09-15 16:09:44 +0330

    - Add game board

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,41 @@
 extern crate termion;
 
-use termion::color;
+use std::io::{self, Write};
+use std::vec;
+use termion::raw::IntoRawMode;
+use termion::input::TermRead;
 
+const BOARD_WIDTH: usize = 10;
+const BOARD_HEIGHT: usize = 20;
 
 fn main(){
-    println!("{red}more red than any comrade{reset} test and {yellow} yellow now",
-    red   = color::Fg(color::Red),
-    reset = color::Fg(color::Reset),
-    yellow = color::Fg(color::Yellow));
+    let mut game_board: Vec<Vec<char>> = vec![vec![' '; BOARD_WIDTH]; BOARD_HEIGHT];
+
+
+
+    let stdout = io::stdout();
+    // ToDo: Handle unwrap creacefully without panic
+    let mut stdout = stdout.lock().into_raw_mode().unwrap();
+
+    print_board(&game_board);
+    for c in io::stdin().keys() {
+        // Handle user input here
+
+        // Update game state here
+
+        // Render the game here
+
+        // Check for game over here
+
+        // Exit game loop on game over
+        if true{
+            break;
+        }
+    }
 }
+fn print_board(board: &Vec<Vec<char>>) {
+    for row in board.iter() {
+        let row_str: String = row.iter().collect();
+        println!("*");
+    }
+}   
